d3b519acf2cbb54c617a1a353365a4b922f51a70
tiioka <t_9642ji@yahoo.co.jp>
2019-04-29 05:11:14 +0900

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%(9!!&4s=kl40gkvy+2-bj)o^@b0ntoqyo(6nfcnu_!5&9q9-e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
