b09e0d6c1356bc5c4931398a9eb3b4760747bbda
tiioka <t_9642ji@yahoo.co.jp>
2019-04-29 04:48:19 +0900

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ import os
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%(9!!&4s=kl40gkvy+2-bj)o^@b0ntoqyo(6nfcnu_!5&9q9-e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
